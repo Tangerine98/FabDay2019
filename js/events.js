897f5f6cf377044324327306a8e202f68203dca1
Tangerine98 <menonnandini98@gmail.com>
2019-04-21 00:11:21 +0530

    Very minor changes

--- js/events.js
+++ js/events.js
@@ -7,3 +7,19 @@ $(function(){
         startAt:		1
     })
 });
+
+jQuery(document).ready(function($) {
+    var changeClass = function() {
+      var windowSize = document.body.clientWidth;
+      if (windowSize < 640) {
+        $('#dates').addClass('timeline-in-mobile');
+      } else if (windowSize >= 641) {
+        $('#dates').removeClass('timeline-in-mobile');
+      };
+    };
+    $(window).resize(function(){
+      changeClass();
+    });
+    //Fire it when the page first loads:
+    changeClass();
+  });
